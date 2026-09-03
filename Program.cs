fa909c1e38858e61d61002f1d717fe643c97ef0b
Mennz <davi.mendoza.rezende.c@gmail.com>
2026-09-03 11:02:57 -0300

    testa todas as formas no Program

--- Program.cs
+++ Program.cs
@@ -1,1 +1,14 @@
-Console.WriteLine("estudando interfaces em C#");
+List<IFormaGeometrica> formas = new List<IFormaGeometrica>
+{
+    new Circulo(5),
+    new Retangulo(4, 6),
+    new Triangulo(3, 4, 5, 4, 3)
+};
+
+foreach (var forma in formas)
+{
+    Console.WriteLine(forma.GetType().Name);
+    Console.WriteLine("area: " + forma.CalcularArea());
+    Console.WriteLine("perimetro: " + forma.CalcularPerimetro());
+    Console.WriteLine();
+}
